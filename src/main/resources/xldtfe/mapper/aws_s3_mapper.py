3ca8aa77f4de05266bc9516bd11e7b3f22dbd065
Benoit Moussaud <bmoussaud@xebialabs.com>
2020-01-03 10:24:04 +0100

    define structured providers for AWS & Azure Public CLoud

--- src/main/resources/xldtfe/mapper/aws_s3_mapper.py
+++ src/main/resources/xldtfe/mapper/aws_s3_mapper.py
@@ -18,21 +18,27 @@ class AWSS3Mapper(ResourceMapper):
     def accepted_type(self):
         return 'aws_s3_bucket'
 
-    def create_ci(self, tf_resource, folder, deployed):        
-        if not self.types_supported(tf_resource['type']):           
+    def create_ci(self, tf_resource, folder, deployed):
+        if not self.types_supported(tf_resource['type']):
             return None
-        self.attributes = tf_resource['attributes']            
-        print("Creating CI of type 'aws.Cloud") 
+        self.attributes = tf_resource['attributes']
+        print("Creating CI of type 'aws.Cloud")
         aws_id = "{0}/{1}".format(folder,"MyAWS")
         print(aws_id)
-        aws_properties = {
-            'accesskey':deployed.container.credentials['AWS_ACCESS_KEY_ID'],
-            'accessSecret': deployed.container.credentials['AWS_SECRET_ACCESS_KEY']
-            }
+        if deployed.container.type == 'terraformEnterprise.AwsProvider':
+            aws_properties = {
+                'accesskey':deployed.container.accesskey,
+                'accessSecret': deployed.container.accessSecret
+                }
+        else:
+            aws_properties = {
+                'accesskey':deployed.container.credentials['AWS_ACCESS_KEY_ID'],
+                'accessSecret': deployed.container.credentials['AWS_SECRET_ACCESS_KEY']
+                }
 
         s3_id = "{0}/{1}".format(aws_id,self.attributes['bucket_domain_name'])
         #print s3_id
-        s3_bucket_properties = {'bucketName':self.attributes['bucket'],'region':self.attributes['region']}    
+        s3_bucket_properties = {'bucketName':self.attributes['bucket'],'region':self.attributes['region']}
         #print s3_bucket_properties
         return [
             super(AWSS3Mapper, self)._create_ci("aws.Cloud", aws_id, aws_properties),
--- src/main/resources/xldtfe/set_environment.py
+++ src/main/resources/xldtfe/set_environment.py
@@ -18,6 +18,20 @@ ws_id = myapi.workspaces.get_id(workspace_name)
 # Inject -no-color options to get no formating in  the logs.
 myapi.variables.create(ws_id,'TF_CLI_ARGS','-no-color','env','false')
 myapi.variables.create(ws_id,'CONFIRM_DESTROY','1','env','false')
+
+for cpm_key in deployed.container.credentialsPropertyMapping:
+    if cpm_key == "empty":
+        continue
+    key = deployed.container.credentialsPropertyMapping[cpm_key]
+    value = deployed.container.getProperty(cpm_key)
+    pd_key = deployed.container.type.getDescriptor().getPropertyDescriptor(cpm_key)
+    if pd_key.isPassword():
+        print("new env sensitive variable {0} -> xxxxxxx".format(key))
+        myapi.variables.create(ws_id,key,value,'env','true')
+    else:
+        print("new env variable {0} -> {1}".format(key,value))
+        myapi.variables.create(ws_id,key,value,'env','false')
+
 for key in deployed.container.variables:
     value = deployed.container.variables[key]
     print("new env variable {0} -> {1}".format(key,value))
