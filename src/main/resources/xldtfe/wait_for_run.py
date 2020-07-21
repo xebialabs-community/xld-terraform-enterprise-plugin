8d06e7f7c1558d6e731b0552e49ce36cd78281e4
Benoit Moussaud <bmoussaud@xebialabs.com>
2020-07-21 11:18:09 +0200

    manage canceled task

--- src/main/resources/xldtfe/wait_for_run.py
+++ src/main/resources/xldtfe/wait_for_run.py
@@ -8,7 +8,7 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-    #
+#
 # Copyright 2019 XEBIALABS
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -24,84 +24,88 @@ import sys
 import json
 import time
 
+
 def dump_json(data, message):
     if True:
-        print(50*'=')
+        print(50 * '=')
         print(message)
-        print(50*'=')
+        print(50 * '=')
         json.dump(data, sys.stdout, indent=4)
-        print(50*'=')
+        print(50 * '=')
 
-def stream_plan_output(myapi,run_id, message):
-    #TODO: use the stream api to dump the archivist logs continuously
-    print(50*'=')
+
+def stream_plan_output(myapi, run_id, message):
+    # TODO: use the stream api to dump the archivist logs continuously
+    print(50 * '=')
     print(message)
-    print(50*'=')
+    print(50 * '=')
     s_plan = myapi.runs.show_plan(run_id)
     status = s_plan['data']['attributes']['status']
     archivist_url = s_plan['data']['attributes']['log-read-url']
     while status == 'running':
-        print "--> status {0}".format(status)
+        print
+        "--> status {0}".format(status)
         time.sleep(5)
         s_plan = myapi.runs.show_plan(run_id)
         status = s_plan['data']['attributes']['status']
 
-    print(50*'=')
-    if status  == 'errored':
-        [sys.stderr.write(line+"\n") for line in myapi.runs.show_plan_log(run_id).split('\n')]
+    print(50 * '=')
+    if status == 'errored':
+        [sys.stderr.write(line + "\n") for line in myapi.runs.show_plan_log(run_id).split('\n')]
         raise Exception("Error during the plan phase")
-    if status  == 'finished':
-        print myapi.runs.show_plan_log(run_id)
-    print(50*'=')
+    if status == 'finished':
+        print
+        myapi.runs.show_plan_log(run_id)
+    print(50 * '=')
 
 
-def stream_apply_output(myapi,run_id, message):
-    #TODO: use the stream api to dump the archivist logs continuously
-    print(50*'=')
+def stream_apply_output(myapi, run_id, message):
+    # TODO: use the stream api to dump the archivist logs continuously
+    print(50 * '=')
     print(message)
-    print(50*'=')
+    print(50 * '=')
     s_apply = myapi.runs.show_apply(run_id)
     status = s_apply['data']['attributes']['status']
     archivist_url = s_apply['data']['attributes']['log-read-url']
     while status == 'running':
-        print "--> status {0}".format(status)
+        print
+        "--> status {0}".format(status)
         time.sleep(5)
         s_apply = myapi.runs.show_apply(run_id)
         status = s_apply['data']['attributes']['status']
 
-    print(50*'=')
-    if status  == 'errored':
-        [sys.stderr.write(line+"\n") for line in myapi.runs.show_apply_log(run_id).split('\n')]
+    print(50 * '=')
+    if status == 'errored':
+        [sys.stderr.write(line + "\n") for line in myapi.runs.show_apply_log(run_id).split('\n')]
         raise Exception("Error during the Apply phase")
-    if status  == 'finished':
-        print myapi.runs.show_apply_log(run_id)
-    print(50*'=')
-
-
+    if status == 'finished':
+        print
+        myapi.runs.show_apply_log(run_id)
+    print(50 * '=')
 
 
 myapi = TFE(organization)
-run_id = context.getAttribute(workspace_name+"_run_id")
+run_id = context.getAttribute(workspace_name + "_run_id")
 print(run_id)
 while True:
-    show_result=myapi.runs.show(run_id)
-    #dump_json(show_result,"show_result")
+    show_result = myapi.runs.show(run_id)
+    # dump_json(show_result,"show_result")
     run = show_result['data']
-    #dump_json(run,"run")
+    # dump_json(run,"run")
     run_status = run['attributes']['status']
     print("run_status: {0}     {1}".format(run_id, run_status))
 
     if run_status == 'planning':
-        stream_plan_output(myapi,run_id, "Plan Log {0}/{1}".format(run_status,run_id))
+        stream_plan_output(myapi, run_id, "Plan Log {0}/{1}".format(run_status, run_id))
 
     if run_status == 'applying':
-        stream_apply_output(myapi,run_id, "Apply Log {0}/{1}".format(run_status,run_id))
+        stream_apply_output(myapi, run_id, "Apply Log {0}/{1}".format(run_status, run_id))
 
     if run_status == 'applied' or run_status == 'planned_and_finished':
         print("done")
         break
-    elif run_status == 'errored':
-        stream_plan_output(myapi,run_id, "Plan Log {0}/{1}".format(run_status,run_id))
+    elif run_status == 'errored' or run_status == 'canceled':
+        stream_plan_output(myapi, run_id, "Plan Log {0}/{1}".format(run_status, run_id))
         raise Exception("An error occured in  {0}".format(run_id))
 
     time.sleep(1)
